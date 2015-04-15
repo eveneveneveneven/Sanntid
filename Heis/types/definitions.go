c56776ea2423f21d4bfdab38637ed382e23614b8
Even <eveneveneveneven@gmail.com>
2015-04-15 13:34:50 +0200

    Updated the cost-function

---
+++ Heis/types/definitions.go
@@ -0,0 +1,40 @@
+package types
+
+// Constant parameters for the network
+const (
+	UDP_PORT = 20011
+	TCP_PORT = 30011
+
+	SEND_INTERVAL       = 250 // milliseconds
+	BUFFER_MSG_RECIEVED = 20
+)
+
+// Type definitions for the elevator and networkmessage protocol
+const (
+	UP int = iota
+	DOWN
+	STOP
+)
+
+const (
+	BUTTON_CALL_UP int = iota
+	BUTTON_CALL_DOWN
+)
+
+type Order struct {
+	ButtonPress int
+	Floor       int
+	Completed   bool
+}
+
+type ElevStat struct {
+	Dir            int
+	Floor          int
+	InternalOrders []bool
+}
+
+type NetworkMessage struct {
+	Id       int
+	Statuses []ElevStat
+	Orders   map[Order]struct{}
+}
