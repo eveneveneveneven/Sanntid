3c861ad0c26ed9dad3aeaa190127508962c76508
edvardsp <edvardsp@github.com>
2015-04-19 22:55:32 +0200

    Can now add internal orders, but does have problems removing them. will fix.

--- Heis/network/netstatus_manager.go
+++ Heis/network/netstatus_manager.go
@@ -43,11 +43,42 @@ func (ns *netStatManager) parseNewMsg(msg *types.NetworkMessage) {
 	ns.netstat.Statuses[id] = msg.Statuses[id]
 	for order := range msg.Orders {
 		if order.Completed {
-			order.Completed = false
-			delete(ns.netstat.Orders, order)
-		} else if _, ok := ns.netstat.Orders[order]; !ok {
-			ns.netstat.Orders[order] = struct{}{}
+			ns.deleteOrder(id, &order)
+		} else {
+			ns.addOrder(id, &order)
 		}
+	}
+}
+
+func (ns *netStatManager) addOrder(id int, order *types.Order) {
+	if order.ButtonPress == types.BUTTON_INTERNAL {
+		newEtg := order.Floor
+		internal := ns.netstat.Statuses[id].InternalOrders
+		for i, etg := range internal {
+			if newEtg == etg {
+				break
+			} else if etg == -1 {
+				internal[i] = newEtg
+				return
+			}
+		}
+	} else {
+		if _, ok := ns.netstat.Orders[*order]; !ok {
+			ns.netstat.Orders[*order] = struct{}{}
+		}
+	}
+}
+
+func (ns *netStatManager) deleteOrder(id int, order *types.Order) {
+	if order.ButtonPress == types.BUTTON_INTERNAL {
+		internal := ns.netstat.Statuses[id].InternalOrders
+		internal = append(internal[1:], -1)
+		elevstat := ns.netstat.Statuses[id]
+		elevstat.InternalOrders = internal
+		ns.netstat.Statuses[id] = elevstat
+	} else {
+		order.Completed = false
+		delete(ns.netstat.Orders, *order)
 	}
 }
 
